e9cfa20882b24d253a01ee4213c1e501f53f29b3
Jooakim <joakim.0565@gmail.com>
2015-11-19 10:27:46 +0100

    Added capability for handling multiple dimensions

--- CalculateSocietyRanking.m
+++ CalculateSocietyRanking.m
@@ -1,11 +1,19 @@
 function societyRanking = CalculateSocietyRanking(societyFitness)
-    societyRanking = zeros(length(societyFitness(1,:)), 1);
     
-    societyFitnessSum = sum(societyFitness);
-   
-    % Calculates the ranking in an evaluation of sort
-    % the minus sign makes the the sort in descending order.
-    fitnessRankings = floor(tiedrank(-societyFitnessSum));
+
+    nbrOfDimensions = length(societyFitness(1,1,:));
+    societyRanking = zeros(1, length(societyFitness(1,:,1)), nbrOfDimensions);
+    
+    
+    for d = 1:nbrOfDimensions
+        societyFitnessForDimension = societyFitness(:,:,d);
+        societyFitnessSum = sum(societyFitnessForDimension);
+
+        % Calculates the ranking in an evaluation of sort
+        % the minus sign makes the the sort in descending order.
+        societyRanking(1,:,d) = floor(tiedrank(-societyFitnessSum));
+    
+    end
     
 
 end
